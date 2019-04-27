3281ed44e7514eaf6235a5cbef86125f97752674
CiyaZ <ciyaz@qq.com>
2019-04-27 17:10:13 +0800

    :hammer: 优化字体显示

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ $kv_map = json_decode($index, true)["kvs"];
 $title = "CiyaZ的笔记系统";
 $bread = "主页";
 if (isset($_GET["id"]) && !empty($_GET["id"])) {
-    if (array_key_exists($_GET["id"], $kv_map)) {
-        $note_path = $kv_map[$_GET["id"]];
-        $note_path_arr = explode("/", $note_path);
-        $title = end($note_path_arr);
-        array_shift($note_path_arr);
-        $bread = implode(" > ", $note_path_arr);
-    } else {
-        http_response_code(404);
-        $title = "404";
-        $bread = "找不到该页面";
-    }
+	if (array_key_exists($_GET["id"], $kv_map)) {
+		$note_path = $kv_map[$_GET["id"]];
+		$note_path_arr = explode("/", $note_path);
+		$title = end($note_path_arr);
+		array_shift($note_path_arr);
+		$bread = implode(" > ", $note_path_arr);
+	} else {
+		http_response_code(404);
+		$title = "404";
+		$bread = "找不到该页面";
+	}
 }
 echo <<<HTML
 <!DOCTYPE html>
@@ -41,29 +41,29 @@ echo <<<HTML
         </p>
         <ul style="text-align: left; padding: 35px" id="tree-view" class="si-list si-bg-white-lighten-2">
 HTML;
-                foreach ($notebooks as $notebook) {
-                    echo "<li class=\"collapsible\">";
-                    echo "<span>".$notebook["name"]."</span>";
-                    echo "</li>";
-                    echo "<li style=\"display: none\">";
-                    echo "<ul class=\"si-list\" style=\"background-color: #E9F1DF\">";
-                    foreach ($notebook["categories"] as $category) {
-                        echo "<li class=\"collapsible\">";
-                        echo "<span>".$category["name"]."</span>";
-                        echo "</li>";
-                        echo "<li style=\"display: none\">";
-                        echo "<ul class=\"si-list si-bg-white\">";
-                        foreach ($category["posts"] as $post) {
-                            echo "<li>";
-                            echo "<span><a href=\"index.php?id=".$post["key"]."\">".$post["name"]."</a></span>";
-                            echo "</li>";
-                        }
-                        echo "</ul>";
-                        echo "</li>";
-                    }
-                    echo "</ul>";
-                    echo "</li>";
-                }
+foreach ($notebooks as $notebook) {
+	echo "<li class=\"collapsible\">";
+	echo "<span>" . $notebook["name"] . "</span>";
+	echo "</li>";
+	echo "<li style=\"display: none\">";
+	echo "<ul class=\"si-list\" style=\"background-color: #E9F1DF\">";
+	foreach ($notebook["categories"] as $category) {
+		echo "<li class=\"collapsible\">";
+		echo "<span>" . $category["name"] . "</span>";
+		echo "</li>";
+		echo "<li style=\"display: none\">";
+		echo "<ul class=\"si-list si-bg-white\">";
+		foreach ($category["posts"] as $post) {
+			echo "<li>";
+			echo "<span><a href=\"index.php?id=" . $post["key"] . "\">" . $post["name"] . "</a></span>";
+			echo "</li>";
+		}
+		echo "</ul>";
+		echo "</li>";
+	}
+	echo "</ul>";
+	echo "</li>";
+}
 echo <<<HTML
         </ul>
         <p>
@@ -85,44 +85,44 @@ echo <<<HTML
     <div class="si-panel si-bg-white-lighten-2" style="margin-top: 5px">
 HTML;
 
-        require "vendor/autoload.php";
-        if (isset($_GET["id"]) && !empty($_GET["id"])) {
-            $post_rendered = render_post($kv_map, $_GET["id"]);
-            if($post_rendered != null) {
-                echo $post_rendered;
-            } else {
-                echo "<div style=\"text-align: center\"><h1>404</h1></div>";
-            }
-        } else {
-            echo render_index();
-        }
+require "vendor/autoload.php";
+if (isset($_GET["id"]) && !empty($_GET["id"])) {
+	$post_rendered = render_post($kv_map, $_GET["id"]);
+	if ($post_rendered != null) {
+		echo $post_rendered;
+	} else {
+		echo "<div style=\"text-align: center\"><h1>404</h1></div>";
+	}
+} else {
+	echo render_index();
+}
 
-        function render_index()
-        {
-            $md = file_get_contents("doc/README.md");
-            $parser = new Parsedown();
-            return $parser->text($md);
-        }
+function render_index()
+{
+	$md = file_get_contents("doc/README.md");
+	$parser = new Parsedown();
+	return $parser->text($md);
+}
 
-        function render_post($kv_map, $hash_key)
-        {
-            if (array_key_exists($hash_key, $kv_map)) {
-                $note_path = $kv_map[$hash_key];
-                $note_md_path = get_md_path($note_path);
-                $md = file_get_contents($note_md_path);
-                $md = preg_replace("/\!\[\]\((.*)\)/", '![](' . $note_path . '/${1})', $md);
-                $parser = new Parsedown();
-                return $parser->text($md);
-            } else {
-                return null;
-            }
-        }
+function render_post($kv_map, $hash_key)
+{
+	if (array_key_exists($hash_key, $kv_map)) {
+		$note_path = $kv_map[$hash_key];
+		$note_md_path = get_md_path($note_path);
+		$md = file_get_contents($note_md_path);
+		$md = preg_replace("/\!\[\]\((.*)\)/", '![](' . $note_path . '/${1})', $md);
+		$parser = new Parsedown();
+		return $parser->text($md);
+	} else {
+		return null;
+	}
+}
 
-        function get_md_path($note_path)
-        {
-            $note_path_arr = explode("/", $note_path);
-            return $note_path . "/" . end($note_path_arr) . ".md";
-        }
+function get_md_path($note_path)
+{
+	$note_path_arr = explode("/", $note_path);
+	return $note_path . "/" . end($note_path_arr) . ".md";
+}
 
 echo <<<HTML
     </div>
@@ -143,6 +143,14 @@ echo <<<HTML
         });
     });
 </script>
+<style>
+code {
+	font-family: "source code variable","consolas","menlo","monaco","courier new",monospace;
+}
+body {
+	font-family: "pingfang sc", "helvetica neue", "hiragino sans gb", "segoe ui", "microsoft yahei ui", 微软雅黑, sans-serif;
+}
+</style>
 </body>
 </html>
 HTML;
